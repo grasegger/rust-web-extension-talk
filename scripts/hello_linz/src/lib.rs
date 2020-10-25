d9d9d56a01073d8e3e8a33c1b461d858ad4da1f7
Karl Grasegger <51945878+grasegger@users.noreply.github.com>
2020-10-25 12:26:12 +0100

    `cargo fmt`

--- scripts/hello_linz/src/lib.rs
+++ scripts/hello_linz/src/lib.rs
@@ -1,9 +1,8 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console::log_1;
 use wasm_bindgen::JsValue;
+use web_sys::console::log_1;
 
 #[wasm_bindgen(start)]
-pub fn main () {
-    log_1(&JsValue::from("Hello, Linz!"));    
+pub fn main() {
+    log_1(&JsValue::from("Hello, Linz!"));
 }
-
